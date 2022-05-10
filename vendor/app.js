f2e65fc214f8ad34d458f3b346f5f5975a613ba0
Matvey <matvey-khramov@inbox.ru>
2022-05-10 14:28:49 +0300

    init

---
+++ vendor/app.js
@@ -0,0 +1,36 @@
+// console.log('Работает!')
+
+async function getPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept:  'application/json',
+    },
+    method: 'GET',
+  })
+
+  const posts = await response.json()
+  
+  console.log(posts)
+
+  const wrapper = document.getElementById('wrapper')
+
+  posts.forEach( (element) => {
+      const div = document.createElement('div')
+      const span = document.createElement('span')
+      const p = document.createElement('p')
+
+      div.id = 'post-' + element.id
+      div.classList.add('post')
+
+      p.textContent = element.body
+      span.textContent = element.title
+
+      div.prepend(span)
+      div.append(p)
+
+      wrapper.append(div)
+  })
+}
+
+getPosts()
